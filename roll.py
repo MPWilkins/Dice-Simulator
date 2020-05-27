07223b77685d33025c758676f6c0dfcdf5d22ed6
Michael <wil13040@byui.edu>
2020-05-27 09:39:29 -0700

    First attempt at menu

---
+++ roll.py
@@ -0,0 +1,3 @@
+# print ("Which kind of Di are you wanting to roll?" \n)
+def menu() :
+    print ("Which kind of D&D Di are you wanting to roll? \n D4 \n D6 \n D8 \n D10 \n D12 \n D20 \n D100 \n Quit")
